ad4e7e91b38e346fab079e584b2ac757bee75639
Jacob Aicher <jake@anoleimaging.com>
2025-11-07 15:15:01 -0500

    add:restructured contact form for responsiveness
    Added flex box containers for more control over the layout for a more
    responsive design. Placed the form element inside a container so that
    the submit button could be moved within the form component but still be
    placed absolutely relative to the new container.

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -5,48 +5,58 @@ const ContactForm = () => {
     
 
     return (
-        
-        <form className={'w-full h-full block p-10 bottom-0 overflow-hidden'}>
+        <div className={'w-full h-full block p-10 bottom-0 relative box-border'}>
+            <form className={'w-full h-full overflow-hidden flex flex-col'}>
 
-            <div
-            className={'relative w-1/2 pr-10 border-r-2'}
-            >
-
-                <label className={'text-left font-[Roboto] block mt-2'}>Name:</label>
-                <input 
-                className={'w-full border-2 rounded-sm px-1 py-0.5'}
-                type="text" 
-                required
-                ></input>
-
-                <label className={'text-left font-[Roboto] block mt-2'}>Email:</label>
-                <input 
-                className={'w-full border-2 rounded-sm px-1 py-0.5'}
-                type="text" 
-                required
-                ></input>
-
-                <label className={'text-left font-[Roboto] block mt-2'}>Inquiry Type:</label>
-                <select
-                className={'w-full border-2 rounded-sm px-1 py-0.5'}
+                <div
+                className={'relative w-2/3 h-fit pr-10 border-r-2 grow-1 flex flex-col gap-2'}
                 >
-                    <option>Work With Me</option>
-                    <option>Hire Me</option>
-                    <option>Question</option>
-                    <option>Other</option>
-                </select>
+                    <div className="w-full flex flex-row">
+                        <label className={'text-left font-[Roboto] my-2 mr-2'}>Name:</label>
+                        <input 
+                        className={'w-full border-2 rounded-sm px-1 py-0.5'}
+                        type="text"
+                        name="name" 
+                        required
+                        ></input>
+                    </div>
 
-            </div>
+                    <div className="w-full flex flex-row">
+                        <label className={'text-left font-[Roboto] block my-2 mr-2'}>Email:</label>
+                        <input 
+                        className={'w-full border-2 rounded-sm px-1 py-0.5'}
+                        type="text" 
+                        name="email" 
+                        required
+                        ></input>
+                    </div>
 
-            <label className={'text-left font-[Roboto] block mt-2'}>Message:</label>
-            <textarea 
-            className={'w-full h-full border-2 rounded-sm px-1 py-0.5'}
-            required
-            rows={5}
-            cols={50}
-            ></textarea>
+                    <div className="w-full flex flex-row">
+                        <label className={'text-left font-[Roboto] my-2 mr-2 text-nowrap'}>Inquiry Type:</label>
+                        <select
+                        className={'w-full border-2 rounded-sm px-1 py-0.5'}
+                        name="inquiryType" 
+                        >
+                            <option>Work With Me</option>
+                            <option>Hire Me</option>
+                            <option>Question</option>
+                            <option>Other</option>
+                        </select>
+                    </div>
+                </div>
 
-        </form>
+                <label className={'text-left font-[Roboto] block mt-2 mb-1'}>Message:</label>
+                <textarea 
+                className={'w-full h-full border-2 rounded-sm px-1 py-0.5 resize-none'}
+                name="message"
+                required
+               
+                ></textarea>
+
+                <button className={'p-2 absolute block -bottom-13 right-5 bg-amber-300 rounded-sm'}
+                >Submit</button>
+            </form>
+        </div>
 
     )
 
